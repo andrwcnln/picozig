2c726cbaf7e6bcf68c6752bd80460b768b7e7599
Andrew Conlin <andrew@andrewconl.in>
2024-02-05 21:29:29 +0000

    [2024-02-05] Working better now, clears display
    I fixed the issues by sending the control data byte before every data byte. Seems wildly inefficient but it works for now.

--- src/i2c.zig
+++ src/i2c.zig
@@ -12,35 +12,21 @@ const led = gpio.num(25);
 const ADDRESS: u8 = 0x3C;
 const CONTROL_COMMAND: u8 = 0x00;
 const CONTROL_DATA: u8 = 0x40;
-const DISPLAY_OFF: u8 = 0xAE;
-const SET_DISPLAY_CLOCK_DIV: u8 = 0xD5;
-const SET_MULTIPLEX: u8 = 0xA8;
-const SET_DISPLAY_OFFSET: u8 = 0xD3;
-const SET_START_LINE: u8 = 0x40;
-const CHARGE_PUMP: u8 = 0x8D;
-const MEMORY_MODE: u8 = 0x20;
-const SEGREMAP: u8 = 0xA0;
-const COMSCANDEC: u8 = 0xC8;
-const SET_COMPINS: u8 = 0xDA;
-const SET_CONSTRAST: u8 = 0xDA;
-const SET_PRECHARGE: u8 = 0xD9;
-const SET_VCOMDETECT: u8 = 0xDB;
-const SET_DISPLAY_ALLON_RESUME: u8 = 0xA4;
-const SET_NORMAL_DISPLAY: u8 = 0xA6;
+const COMMAND_ADDRESSING_MODE = 0x20;
 const COMMAND_COLUMN_ADDRESS = 0x21;
 const COMMAND_PAGE_ADDRESS = 0x22;
 
-const INIT = [_]u8{ CONTROL_COMMAND, 0xAE, CONTROL_COMMAND, 0xA8, 0x3F, CONTROL_COMMAND, 0xD3, 0x00, CONTROL_COMMAND, 0x40, CONTROL_COMMAND, 0xA0, CONTROL_COMMAND, 0xC0, CONTROL_COMMAND, 0xDA, 0x02, CONTROL_COMMAND, 0x81, 0x7F, CONTROL_COMMAND, 0xA4, CONTROL_COMMAND, 0xD5, 0x80, CONTROL_COMMAND, 0x8D, 0x14, CONTROL_COMMAND, 0xAF };
+const INIT = [_]u8{ CONTROL_COMMAND, 0xAE, CONTROL_COMMAND, 0xA8, 0x1F, CONTROL_COMMAND, 0xD3, 0x00, CONTROL_COMMAND, 0x40, CONTROL_COMMAND, 0xA0, CONTROL_COMMAND, 0xC0, CONTROL_COMMAND, 0xDA, 0x02, CONTROL_COMMAND, 0x81, 0x7F, CONTROL_COMMAND, 0xA4, CONTROL_COMMAND, 0xD5, 0x80, CONTROL_COMMAND, 0x8D, 0x14, CONTROL_COMMAND, 0xAF };
 
 const DRAW = [_]u8{
     CONTROL_COMMAND,
     COMMAND_COLUMN_ADDRESS,
     0x00,
-    0x01,
+    0x7F,
     CONTROL_COMMAND,
     COMMAND_PAGE_ADDRESS,
     0x00,
-    0x00,
+    0x03,
 };
 
 pub fn main() !void {
@@ -52,35 +38,42 @@ pub fn main() !void {
         .clock_config = rp2040.clock_config,
         .scl_pin = gpio.num(21),
         .sda_pin = gpio.num(20),
-        .baud_rate = 400000,
+        .baud_rate = 100000,
     });
 
     time.sleep_ms(1000);
 
-    try send_command(&INIT);
+    try send(&INIT);
 
-    time.sleep_ms(10000);
+    time.sleep_ms(1000);
     led.put(1);
     time.sleep_ms(1000);
     led.put(0);
 
-    try send_command(&DRAW);
-    try send_data();
+    const set_addressing_mode = [3]u8{ CONTROL_COMMAND, COMMAND_ADDRESSING_MODE, 0b00 };
+    try send(&set_addressing_mode);
+    try send(&DRAW);
+    var x: usize = 0;
+    while (x < 512) : (x += 1) {
+        try send(&[2]u8{ CONTROL_DATA, 0x0 });
+    }
 }
 
-pub fn send_command(bytes: []const u8) !void {
+pub fn send(bytes: []const u8) !void {
     const a: i2c.Address = @enumFromInt(ADDRESS);
     _ = i2c0.write_blocking(a, bytes) catch {
         led.put(1);
     };
 }
-pub fn send_data() !void {
+
+pub fn send_data(data: u8) !void {
     const a: i2c.Address = @enumFromInt(ADDRESS);
     const DATA = [1]u8{CONTROL_DATA};
     _ = i2c0.write_blocking(a, &DATA) catch {
         led.put(1);
     };
-    const page_buffer = [_]u8{ 0xFF, 0xFF };
+    var page_buffer: [28]u8 = undefined;
+    @memset(&page_buffer, data);
     _ = i2c0.write_blocking(a, &page_buffer) catch {
         led.put(1);
     };
